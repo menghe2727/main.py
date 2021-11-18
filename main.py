36530ad31ab26ce8c27c868f2be5581bb32a100f
unknown <menghe2727@gmail.com>
2021-11-18 16:44:48 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import numpy as np
+import pandas as pd
+import os
+train_data=pd.read_csv("D:/kaggle/train.csv");
+test_data=pd.read_csv("D:/kaggle/test.csv");
+print(train_data)
+print(test_data)
+
